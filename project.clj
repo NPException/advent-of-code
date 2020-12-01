a6ac794dc228c190e51c730689f2d27ef4edca71
Dirk Wetzel <npexception42@gmail.com>
2020-12-01 14:49:00 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject advent-of-code "0.1.0-SNAPSHOT"
+  :license {:name "MIT License"
+            :url "https://en.wikipedia.org/wiki/MIT_License"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
