4aae3a09529c70ba538166d9921697320a58a38b
NPException <npexception42@gmail.com>
2020-12-02 17:19:25 +0100

    2020 Day 2 - switch to re-seq

--- src/aoc_2020/day_2.clj
+++ src/aoc_2020/day_2.clj
@@ -1,27 +1,24 @@
 (ns aoc-2020.day-2
-  (:require [clojure.java.io :as io]
-            [clojure.string :as string]))
+  (:require [clojure.java.io :as io]))
 
 ;; --- Day 2: Password Philosophy --- https://adventofcode.com/2020/day/2
 
 (def task-input
-  (-> (io/resource "inputs/aoc-2020/day-2.txt")
-      slurp))
+  (slurp (io/resource "inputs/aoc-2020/day-2.txt")))
 
 
 (defn line->rule-set
-  [line]
-  (when-let [[_ n1 n2 c password] (re-matches #"(\d+)-(\d+) (.): (.+)" line)]
-    [(Integer/parseInt n1)
-     (Integer/parseInt n2)
-     (first c)
-     password]))
+  [[_ n1 n2 c password]]
+  [(Integer/parseInt n1)
+   (Integer/parseInt n2)
+   (first c)
+   password])
 
 
 (defn count-valid-passwords
   [input valid-password?]
   (->> input
-       string/split-lines
+       (re-seq #"(\d+)-(\d+) (.): (.+)")
        (map line->rule-set)
        (filter #(apply valid-password? %))
        count))
