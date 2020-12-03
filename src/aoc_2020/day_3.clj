e6dcf21ad95e6a02bd2d66df84c97d066405ef7b
NPException <npexception42@gmail.com>
2020-12-03 18:33:02 +0100

    2020 Day 3

---
+++ src/aoc_2020/day_3.clj
@@ -0,0 +1,49 @@
+(ns aoc-2020.day-3
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+;; --- Day 3: Toboggan Trajectory --- https://adventofcode.com/2020/day/3
+
+(def task-input
+  (slurp (io/resource "inputs/aoc-2020/day-3.txt")))
+
+
+(defn count-trees
+  [input slope-right slope-down]
+  (let [lines (string/split-lines input)
+        w (count (first lines))]
+    (->> lines
+         ;; check down
+         (map-indexed vector)
+         (filter #(= 0 (mod (first %) slope-down)))
+         ;; check right
+         (map-indexed (fn [i [_ line]]
+                        [(* slope-right i) line]))
+         (filter (fn [[pos line]]
+                   (= \# (nth line (mod pos w)))))
+         count)))
+
+
+;; alternative approach
+(defn count-trees-2
+  [input right down]
+  (let [lines (string/split-lines input)
+        width (count (first lines))]
+    (->> (range (count lines))
+         (filter #(= 0 (mod % down)))
+         (map #(get-in lines [% (-> % (/ down) (* right) (mod width))]))
+         (filter #(= \# %))
+         count)))
+
+
+(comment
+  ;; Part 1
+  (count-trees task-input 3 1)
+
+  ;; Part 2
+  (* (count-trees task-input 1 1)
+     (count-trees task-input 3 1)
+     (count-trees task-input 5 1)
+     (count-trees task-input 7 1)
+     (count-trees task-input 1 2))
+  )
