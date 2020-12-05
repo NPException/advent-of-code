853fa2d9655c15d27dfd9ad050be16c7362e15cb
NPException <npexception42@gmail.com>
2020-12-05 10:43:33 +0100

    2020 Day 5 - Part 2 - improvements

--- src/aoc_2020/day_5.clj
+++ src/aoc_2020/day_5.clj
@@ -4,28 +4,21 @@
 
 ;; --- Day 5: Binary Boarding --- https://adventofcode.com/2020/day/5
 
-(def task-input
-  (u/slurp-resource "inputs/aoc_2020/day-5.txt"))
-
-
 (defn parse-seat-id
   [seat]
   (-> (string/replace seat #"." {"F" "0", "B" "1", "L" "0", "R" "1"})
       (Integer/parseInt 2)))
 
 
-(defn find-highest-seat-id
-  [input]
-  (->> (string/split-lines input)
-       (map parse-seat-id)
-       (apply max)))
+(def all-ids
+  (->> (u/slurp-resource "inputs/aoc_2020/day-5.txt")
+       string/split-lines
+       (mapv parse-seat-id)))
 
 
 (defn find-my-seat
-  [input]
-  (->> (string/split-lines input)
-       (map parse-seat-id)
-       sort
+  [ids]
+  (->> (sort ids)
        (#(interleave % (rest %)))
        (partition 2)
        (filter (fn [[a b]] (not= (inc a) b)))
@@ -33,9 +26,18 @@
        inc))
 
 
+;; more efficient approach (credits to Jezza)
+(defn find-my-seat-2
+  [ids]
+  (->> (range (apply min ids) (apply max ids))
+       (filter (complement (set ids)))
+       first))
+
+
+
 (comment
   ;; Part 1
-  (find-highest-seat-id task-input)
+  (apply max all-ids)
   ;; Part 2
-  (find-my-seat task-input)
+  (find-my-seat all-ids)
   )
