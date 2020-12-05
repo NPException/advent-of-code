061ac46dc43995313ff76cfaed84e3ff37d5c3c8
NPException <npexception42@gmail.com>
2020-12-05 10:05:10 +0100

    2020 Day 5 - Part 2

--- src/aoc_2020/day_5.clj
+++ src/aoc_2020/day_5.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as string]
             [aoc-utils :as u]))
 
-;; --- Day 5:  --- https://adventofcode.com/2020/day/5
+;; --- Day 5: Binary Boarding --- https://adventofcode.com/2020/day/5
 
 (def task-input
   (u/slurp-resource "inputs/aoc_2020/day-5.txt"))
@@ -21,9 +21,21 @@
        (apply max)))
 
 
+(defn find-my-seat
+  [input]
+  (->> (string/split-lines input)
+       (map parse-seat-id)
+       sort
+       (#(interleave % (rest %)))
+       (partition 2)
+       (filter (fn [[a b]] (not= (inc a) b)))
+       ffirst
+       inc))
+
+
 (comment
   ;; Part 1
   (find-highest-seat-id task-input)
   ;; Part 2
-
+  (find-my-seat task-input)
   )
