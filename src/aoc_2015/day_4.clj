51d7d57c5f940e064d3bf0ae01b22db06481ee71
NPException <npexception42@gmail.com>
2020-12-06 21:14:31 +0100

    2015 Day 4 - Part 2

--- src/aoc_2015/day_4.clj
+++ src/aoc_2015/day_4.clj
@@ -38,18 +38,26 @@
     (.toString sb)))
 
 
+(defn hash-fn
+  [secret hash-target]
+  (fn [n]
+    (let [hash (md5-fast (str secret n))]
+      (when (string/starts-with? hash hash-target)
+        [n hash]))))
+
+
 (defn mine-advent-coin
-  [secret]
+  [secret hash-target]
   (->> (iterate inc 1)
-       (map (juxt identity (partial str secret)))
-       (map #(vector (% 0) (md5-fast (% 1))))
-       (filter #(string/starts-with? (% 1) "00000"))
+       (u/cpmap 500 (hash-fn secret hash-target))
+       #_(map (hash-fn secret hash-target))                 ;; takes ~6x as long for part 2 compared to u/cpmap
+       (filter some?)
        first))
 
 
 (comment
   ;; Part 1
-  (mine-advent-coin task-input)
+  (mine-advent-coin task-input "00000")
   ;; Part 2
-
+  (mine-advent-coin task-input "000000")
   )
