6d2d80cf825f95b26dec568fb52f80cbf9b90f0c
NPException <npexception42@gmail.com>
2020-12-12 20:54:32 +0100

    2015 Day 6 - Part 1 - welcome back, BitSet! :D

--- src/clojure/aoc_2015/day_6.clj
+++ src/clojure/aoc_2015/day_6.clj
@@ -1,6 +1,7 @@
 (ns aoc-2015.day-6
   (:require [clojure.string :as string]
-            [aoc-utils :as u]))
+            [aoc-utils :as u])
+  (:import [java.util BitSet]))
 
 ;; --- Day 6: Probably a Fire Hazard --- https://adventofcode.com/2015/day/6
 
@@ -22,31 +23,28 @@
 
 (defn toggle-lights
   [data [action x1 y1 x2 y2]]
-  (letfn [(target? [i]
-            (and (<= x1 (mod i 1000) x2)
-                 (<= y1 (quot i 1000) y2)))]
+  (let [x-end (inc x2)
+        y-end (inc y2)]
     (case action
-      "turn on" (into []
-                      (map-indexed #(if (target? %1) true %2))
-                      data)
-      "turn off" (into []
-                       (map-indexed #(if (target? %1) false %2))
-                       data)
-      "toggle" (into []
-                     (map-indexed #(if (target? %1) (not %2) %2))
-                     data))))
+      "turn on" (dotimes [oy (- y-end y1)]
+                  (.set ^BitSet (data (+ y1 oy)) ^int x1 ^int x-end))
+      "turn off" (dotimes [oy (- y-end y1)]
+                   (.clear ^BitSet (data (+ y1 oy)) ^int x1 ^int x-end))
+      "toggle" (dotimes [oy (- y-end y1)]
+                 (.flip ^BitSet (data (+ y1 oy)) ^int x1 ^int x-end))))
+  data)
 
 
 (defn part-1
   [input]
-  (let [start-data (vec (repeat (* 1000 1000) false))
+  (let [start-data (vec (repeatedly 1000 #(BitSet. 1000)))
         final-data (reduce
                      toggle-lights
                      start-data
                      (parse-instructions input))]
     (->> final-data
-         (filter true?)
-         count)))
+         (map #(.cardinality ^BitSet %))
+         (apply +))))
 
 
 (defn part-2
@@ -56,7 +54,7 @@
 
 (comment
   ;; Part 1
-  (part-1 task-input)
+  (part-1 task-input)                                       ; => 400410
 
   ;; Part 2
   (part-2 task-input)
