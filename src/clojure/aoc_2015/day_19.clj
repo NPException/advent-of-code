1d59b2f73f65992874fbdb10526f5fe176b605c5
NPException <npexception42@gmail.com>
2022-11-19 23:09:11 +0100

    2015 Day 19 part 1

--- src/clojure/aoc_2015/day_19.clj
+++ src/clojure/aoc_2015/day_19.clj
@@ -1,10 +1,10 @@
 (ns aoc-2015.day-19
-  (:use [criterium.core])
   (:require [clojure.string :as str]
-            [clojure.java.math :as math]
+            [clojure.math :as math]
+            [criterium.core :as crit]
             [aoc-utils :as u]))
 
-;; --- Day 19:  --- https://adventofcode.com/2015/day/19
+;; --- Day 19: Medicine for Rudolph --- https://adventofcode.com/2015/day/19
 
 (def task-input (u/slurp-resource "inputs/aoc_2015/day-19.txt"))
 
@@ -12,10 +12,37 @@
 (def test-input-2 "H => HO\nH => OH\nO => HH\n\nHOHOHO")
 
 
+(defn parse-input
+  [input]
+  (let [[replacement-lines
+         [_ molecule]] (->> (str/split-lines input)
+                            (split-with seq))]
+    [(mapv #(str/split % #" => ") replacement-lines)
+     molecule]))
+
+
+(defn replace-every
+  [^String src ^String target ^String replacement]
+  (iteration
+    (fn [^long start-index]
+      (let [found-index (.indexOf src target start-index)]
+        (when-not (neg? found-index)
+          [(inc found-index)
+           (str (subs src 0 found-index)
+             replacement
+             (subs src (+ found-index (.length target)) (.length src)))])))
+    :initk 0
+    :kf #(nth % 0)
+    :vf #(nth % 1)))
+
 
 (defn part-1
   [input]
-  )
+  (let [[replacements molecule] (parse-input input)]
+    (->> replacements
+         (mapcat #(apply replace-every molecule %))
+         set
+         count)))
 
 
 (defn part-2
@@ -27,13 +54,13 @@
   ;; Part 1
   (part-1 test-input-1)                                     ; => 4
   (part-1 test-input-2)                                     ; => 7
-  (part-1 task-input)                                       ; =>
-  (quick-bench (part-1 task-input))
+  (part-1 task-input)                                       ; => 509
+  (crit/quick-bench (part-1 task-input))
 
   ;; Part 2
   (part-2 test-input-1)                                     ; =>
   (part-2 test-input-2)                                     ; =>
   (part-2 task-input)                                       ; =>
-  (quick-bench (part-2 task-input))
+  (crit/quick-bench (part-2 task-input))
 
   )
