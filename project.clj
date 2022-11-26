b292e0a69f9ee983dca16f5f8c3e77886814dcc3
NPException <npexception42@gmail.com>
2022-11-26 09:34:51 +0100

    disable default boxed-math and reflection warnings
    moved them to comments on top of the template namespace instead

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject advent-of-code "0.1.0-SNAPSHOT"
   :license {:name "MIT License"
             :url "https://en.wikipedia.org/wiki/MIT_License"}
-  :global-vars {*warn-on-reflection* true
-                *unchecked-math* :warn-on-boxed}
   :repl-options {:init-ns aoc-utils}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.match "1.0.0"]
