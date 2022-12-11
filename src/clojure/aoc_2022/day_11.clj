af07317032bc84fd9d1b2ee071594a2a4de45575
NPException <npexception42@gmail.com>
2022-12-11 13:34:44 +0100

    2022 Day 11 - Part 2

--- src/clojure/aoc_2022/day_11.clj
+++ src/clojure/aoc_2022/day_11.clj
@@ -21,37 +21,50 @@
 
 (defn parse-monkey
   [text]
-  (let [[_ items operation test if-true if-false]
-        (str/split text #"\n")]
-    (transient
-      (-> []
-          (assoc &items
-            (transient (u/read-as-vector (second (str/split items #"items:")))))
-          (assoc &inspections
-            0)
-          (assoc &inspect-fn
-            (let [[op ^long x] (u/split-parse operation nil #"new = old " str #" " parse-long)]
-              (case [op (some? x)]
-                ["+" true] (fn [^long item] (+ item x))
-                ["+" false] (fn [^long item] (+ item item))
-                ["*" true] (fn [^long item] (* item x))
-                ["*" false] (fn [^long item] (* item item)))))
-          (assoc &aim-fn
-            (let [[^long n] (u/split-parse test nil #"by " parse-long)
-                  [t] (u/split-parse if-true nil #"monkey " parse-long)
-                  [f] (u/split-parse if-false nil #"monkey " parse-long)]
-              #(if (= 0 (rem ^long % n)) t f)))))))
+  (let [[_ items operation test if-true if-false] (str/split text #"\n")]
+    (-> []
+        (assoc &items
+          (u/read-as-vector (second (str/split items #"items:"))))
+        (assoc &inspections
+          0)
+        (assoc &inspect-fn
+          (u/split-parse operation nil #"new = old " str #" " parse-long))
+        (assoc &aim-fn
+          (let [[^long n] (u/split-parse test nil #"by " parse-long)
+                [t] (u/split-parse if-true nil #"monkey " parse-long)
+                [f] (u/split-parse if-false nil #"monkey " parse-long)]
+            [n t f])))))
+
+
+(defn activate-monkey
+  [monkey]
+  (-> monkey
+      (update &items transient)
+      (update &inspect-fn (fn [[op ^long x]]
+                            (case [op (some? x)]
+                              ["+" true] (fn [^long item] (+ item x))
+                              ["+" false] (fn [^long item] (+ item item))
+                              ["*" true] (fn [^long item] (* item x))
+                              ["*" false] (fn [^long item] (* item item)))))
+      (update &aim-fn (fn [[^long n t f]]
+                        #(if (= 0 (rem ^long % n)) t f)))
+      (transient)))
 
 
 (defn parse-input
   [input]
-  (->> (str/split input #"\n\n")
-       (mapv parse-monkey)))
+  (let [monkeys  (->> (str/split input #"\n\n")
+                      (mapv parse-monkey))
+        magic (->> monkeys
+                      (map #(nth % &aim-fn))
+                      (map first)
+                      (apply *))]
+    [magic (mapv activate-monkey monkeys)]))
 
 
 (defn item-inspection
-  [relief-fn inspect-fn aim-fn monkeys item]
-  (let [item'         (inspect-fn item)
+  [cap-fn relief-fn inspect-fn aim-fn monkeys item]
+  (let [item'         (cap-fn (inspect-fn item))
         item''        (relief-fn item')                     ;; relief / monkey gets bored
         target-index  (aim-fn item'')
         target-monkey (nth monkeys target-index)]
@@ -69,37 +82,39 @@
 
 
 (defn turn
-  [relief-fn monkeys monkey-index]
+  [cap-fn relief-fn monkeys monkey-index]
   (let [monkey  (nth monkeys monkey-index)
         items   (nth monkey &items)
         inspect (nth monkey &inspect-fn)
         aim     (nth monkey &aim-fn)]
     (reduce
-      #(item-inspection relief-fn inspect aim %1 %2)
+      #(item-inspection cap-fn relief-fn inspect aim %1 %2)
       (assoc! monkeys monkey-index
         (update-inspections! monkey (count items)))
       (persistent! items))))
 
 
 (defn round
-  [relief-fn monkeys]
-  (reduce #(turn relief-fn %1 %2)
+  [cap-fn relief-fn monkeys]
+  (reduce #(turn cap-fn relief-fn %1 %2)
     monkeys
     (range (count monkeys))))
 
 
 (defn compute-monkey-business
   [input ^long rounds relief-fn]
-  (->> (parse-input input)
-       (transient)
-       (iterate #(round relief-fn %))
-       (take (inc rounds))
-       (last)
-       (persistent!)
-       (map #(nth % &inspections))
-       (sort #(compare %2 %1))
-       (take 2)
-       (apply *)))
+  (let [[^long magic monkeys] (parse-input input)
+        cap-fn (fn [^long x]
+                 (rem x magic))]
+    (->> (transient monkeys)
+         (iterate #(round cap-fn relief-fn %))
+         (take (inc rounds))
+         (last)
+         (persistent!)
+         (map #(nth % &inspections))
+         (sort #(compare %2 %1))
+         (take 2)
+         (apply *))))
 
 
 (defn part-1
@@ -107,7 +122,6 @@
   (compute-monkey-business input 20 #(quot ^long % 3)))
 
 
-;; TODO: somehow keep the numbers within a confined range, without changing divisibilities
 (defn part-2
   [input]
   (compute-monkey-business input 10000 identity))
@@ -121,7 +135,7 @@
 
   ;; Part 2
   (part-2 test-input)                                       ; => 2713310158
-  (part-2 task-input)                                       ; =>
+  (part-2 task-input)                                       ; => 23612457316
   (crit/quick-bench (part-2 task-input))
 
   )
