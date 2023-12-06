1df11ca8b6b740dd63980764a17cdee543aabdd4
NPException <npexception42@gmail.com>
2023-12-06 16:21:28 +0100

    2023 Day 6 Part 1

---
+++ src/clojure/aoc_2023/day_6.clj
@@ -0,0 +1,61 @@
+(ns aoc-2023.day-6
+  (:require [aoc-utils :as u]
+            [clojure.math :as math]
+            [clojure.string :as str]
+            [criterium.core :as crit]))
+
+; (set! *warn-on-reflection* true)
+; (set! *unchecked-math* :warn-on-boxed)
+
+;; --- Day 6: Wait For It ---
+
+(def task-input (u/slurp-resource "inputs/aoc_2023/day-6.txt"))
+
+(def test-input "Time:      7  15   30\nDistance:  9  40  200")
+
+(defn parse-line
+  [line]
+  (->> (str/split line #":")
+       (second)
+       (u/read-as-vector)))
+
+(defn parse-input
+  [input]
+  (->> (str/split-lines input)
+       (mapv parse-line)))
+
+
+(defn calculate-distance
+  [total-time press-time]
+  (* press-time (- total-time press-time)))
+
+(defn count-winning-strats
+  [time distance]
+  (->> (range 1 time)
+       (mapv #(calculate-distance time %))
+       (u/count-matching #(> % distance))))
+
+
+(defn part-1
+  [input]
+  (let [[times distances] (parse-input input)]
+    (apply * (map count-winning-strats times distances))))
+
+
+(defn part-2
+  [input]
+  )
+
+
+(comment
+  ;; Part 1
+  (part-1 test-input)                                       ; => 288
+  (part-1 task-input)                                       ; => 800280
+  (crit/quick-bench (part-1 task-input))
+
+  ;; Part 2
+  (part-2 test-input)                                       ; =>
+  (part-2 task-input)                                       ; =>
+  (crit/quick-bench (part-2 task-input))
+
+  )
