354dc2d79396e63d103c7c96db638d03c7ca0fd7
NPException <npexception42@gmail.com>
2023-12-15 09:16:41 +0100

    2023 Day 14 Part 2

--- src/clojure/aoc_2023/day_14.clj
+++ src/clojure/aoc_2023/day_14.clj
@@ -14,31 +14,88 @@
 (def test-input "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....")
 
 
+(defn slide-column
+  [grid x]
+  (let [height (count grid)]
+    (loop [grid grid
+           low  0
+           y    0]
+      (if (= y height)
+        grid
+        (case (u/nth-in grid [y x])
+          \. (recur grid low (inc y))
+          \# (recur grid (inc y) (inc y))
+          \O (recur
+               (-> grid
+                   (assoc-in [y x] \.)
+                   (assoc-in [low x] \O))
+               (inc low)
+               (inc y)))))))
+
+(defn slide-rocks
+  [grid]
+  (let [width  (count (first grid))
+        height (count grid)
+        xs     (range width)
+        ys     (range height)]
+    (reduce
+      slide-column
+      grid
+      xs)))
+
+
 (defn column-load
   [grid x]
   (let [height (count grid)]
-    (loop [sum 0
-           y 0
-           max-load height]
-      (if (= y height)
-        sum
-        (case (u/nth-in grid [y x])
-          \. (recur sum (inc y) max-load)
-          \O (recur (+ sum max-load) (inc y) (dec max-load))
-          \# (recur sum (inc y) (dec (- height y))))))))
+    (->> (range height)
+         (filterv #(= \O (u/nth-in grid [% x])))
+         (mapv #(- height %))
+         (apply +))))
+
+
+(defn calculate-load
+  [grid]
+  (->> (range (count (first grid)))
+       (mapv #(column-load grid %))
+       (apply +)))
 
 
 (defn part-1
   [input]
-  (let [grid (str/split-lines input)]
-    (->> (range (count (first grid)))
-         (mapv #(column-load grid %))
-         (apply +))))
+  (->> (str/split-lines input)
+       (mapv vec)
+       (slide-rocks)
+       (calculate-load)))
+
+
+(defn rotate-grid-clockwise
+  [grid]
+  (let [xs  (range (count (first grid)))
+        rys (reverse (range (count grid)))]
+    (->> xs
+         (mapv
+           (fn [x]
+             (mapv #(u/nth-in grid [% x]) rys))))))
 
 
 (defn part-2
   [input]
-  )
+  (let [grid        (->> (str/split-lines input)
+                         (mapv vec))
+        slide-step  #(-> % (slide-rocks) (rotate-grid-clockwise))
+        slide-cycle #(-> % (slide-step) (slide-step) (slide-step) (slide-step))]
+    (loop [step 0
+           prev-grid grid
+           seen {grid step}]
+      (let [grid (slide-cycle prev-grid)]
+        (if-let [last-seen (seen grid)]
+          (let [repeat-size (- step last-seen)
+                steps-needed (mod (- 1000000000 last-seen) repeat-size)]
+            (->> (iterate slide-cycle grid)
+                 (take steps-needed)
+                 (last)
+                 (calculate-load)))
+          (recur (inc step) grid (assoc seen grid step)))))))
 
 
 (comment
@@ -48,8 +105,8 @@
   (crit/quick-bench (part-1 task-input))
 
   ;; Part 2
-  (part-2 test-input)                                       ; =>
-  (part-2 task-input)                                       ; =>
+  (part-2 test-input)                                       ; => 64
+  (part-2 task-input)                                       ; => 100064
   (crit/quick-bench (part-2 task-input))
 
   )
